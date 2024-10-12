5bca04d2d57c717f38327014c5c8871fc3f6f8a0
DmitriyLesnoy <vasymedov@gmail.com>
2024-10-12 16:13:36 +0300

    vault backup: 2024-10-12 16:13:36

--- Programming/Lab1/testa.java
+++ Programming/Lab1/testa.java
@@ -1,12 +1,15 @@
 public class testa {
     public static void main(String[] args) {
-        switch(args.length) {
-            case 0: System.out.println("Hello, world!"); break;
-            case 1: System.out.println("Hello, " + args[0] + "!");
-            case 2: System.out.println("Hello, " + args[0] + " and " + args[1] + "!");
-            default: System.out.println("Hello, all!");
-        }
+        // switch(args.length) {
+        //     case 0: System.out.println("Hello, world!"); break;
+        //     case 1: System.out.println("Hello, " + args[0] + "!");
+        //     case 2: System.out.println("Hello, " + args[0] + " and " + args[1] + "!");
+        //     default: System.out.println("Hello, all!");
+        // }
 
+        int a = 1;
+        foo(a);
+        System.out.print(a);
         // if (args.length == 0) {
         //     System.out.println("Hello, world!");
         // }
@@ -19,6 +22,10 @@ public class testa {
         // if (args.length > 2) {
         //     System.out.println("Hello, all!");
         // }
+    }
 
+    public static void foo(int a){
+        a+=10;
+        // System.out.println(args);
     }
 }
